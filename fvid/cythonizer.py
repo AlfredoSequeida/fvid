03946ac188d96c55e5593423c2cd5291949d3226
Theelgirl <root@lsasdiplomacy.com>
2021-03-02 10:09:07 -0800

    Update docs; Add WIP testing; Speed up Cython

--- fvid/cythonizer.py
+++ fvid/cythonizer.py
@@ -1,6 +1,7 @@
-#cython: language_level = 3
+#cython: language_level=3
+# distutils: include_dirs = /root/fvid, /rood/fvid/tests
 from distutils.core import Extension, setup
 from Cython.Build import cythonize
 
-ext = Extension(name="fvid_cython", sources=["fvid_cython.pyx"])
+ext = Extension(name="fvid_cython", sources=["fvid_cython.pyx"], include_dirs=['/root/fvid', '/root/fvid/tests'])
 setup(ext_modules=cythonize(ext, compiler_directives={'language_level': 3, 'infer_types': True}))
--- fvid/fvid_cython.pyx
+++ fvid/fvid_cython.pyx
@@ -1,45 +1,40 @@
-# distutils: language=c
+# distutils: language=c++
 # cython: boundscheck=False
 # cython: cdivision=True
 # cython: wraparound=False
+# cython: c_string_type=unicode, c_string_encoding=ascii
+
+from libcpp.string cimport string
 
 cpdef str cy_gbfi(image):
     cdef int width, height, x, y
-    cdef str pixel_bin_rep, bits
+    cdef string bits = b''
     cdef (int, int, int) pixel
 
     width, height = image.size
 
     px = image.load()
-    bits = ""
     
     for y in range(height):
         for x in range(width):
             pixel = px[x, y]
 
-            pixel_bin_rep = <str>"0"
-
             # if the white difference is smaller (comparison part 1), that means the pixel is closer
             # to white, otherwise, the pixel must be black
-            if abs(pixel[0] - 255) < abs(pixel[0] - 0) and abs(pixel[1] - 255) < abs(pixel[1] - 0) and abs(pixel[2] - 255) < abs(pixel[2] - 0):
-                pixel_bin_rep = <str>"1"
-
-            # adding bits
-            bits += pixel_bin_rep
+            bits.append(b'1' if abs(pixel[0] - 255) < abs(pixel[0] - 0) and abs(pixel[1] - 255) < abs(pixel[1] - 0) and abs(pixel[2] - 255) < abs(pixel[2] - 0) else b'0')
 
     return bits
 
 cpdef str cy_gbfi_h265(image):
     cdef int width, height, x, y
-    cdef str bits
+    cdef string bits = b''
 
     width, height = image.size
 
     px = image.load()
-    bits = ""
     
     for y in range(height):
         for x in range(width):
-            bits += "1" if px[x, y] == 255 else "0"
+            bits.append(b'1' if px[x, y] == 255 else b'0')
 
     return bits
